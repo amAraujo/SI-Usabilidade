fc418d5e7a0873658cccd0e53d219a165f49a999
Mateus Rodrigues <omateus2004@gmail.com>
2016-11-02 18:54:57 -0300

    Implementação de compra de passagens

--- app/components/home/home-controller.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by mateu on 02/11/2016.
- */
--- assets/js/app.js
+++ assets/js/app.js
@@ -1,4 +1,5 @@
 /**
  * Created by mateu on 02/11/2016.
  */
-angular.module('App',['ngMaterial','ui.router', 'App.router']);
+angular.module('App',['ngMaterial','ui.router', 'App.router', 'App.controllers' , 'AngularPrint']);
+var moduloControlador = angular.module('App.controllers',[]);
